9341079de1d577a7755afa4a3af76ddf2ac820ed
Ganesh Koilada <itsmeganeshcse@gmail.com>
2020-09-18 16:32:03 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 import {NavLink} from 'react-router-dom'
-import Login from './components/Login'
+
 // import Signup from 'components/SignUp/SignUp'
 
 function App() {
